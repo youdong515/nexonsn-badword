9877ef4345d68a859be13d75cdd75779975fdf21
youdong515 <youdong515@naver.com>
2022-10-10 20:51:55 +0900

    update nlp model

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import inspect
 import seaborn as sns
 import pandas as pd 
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
